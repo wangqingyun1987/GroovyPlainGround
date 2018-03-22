06353e633c05ce754b719d2e19df87859242f085
wangqingyun1987 <35253219+wangqingyun1987@users.noreply.github.com>
2018-03-22 08:59:16 +0800

    added some basic usages

---
+++ Book.groovy
@@ -0,0 +1,10 @@
+/*
+	Groovy facilitates working with beans in three ways:
+		1): Generating the accessor methods
+		2): Allowing simplified access to all JavaBeans (including GroovyBeans)
+		3): Simplifying registration of event handlers together with annotations that declare a property as bindable
+*/
+
+class Book {
+	String title
+}
